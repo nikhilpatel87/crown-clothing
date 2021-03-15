354153c0649745fcfe3aa607f2656d925e8a9eb9
Nikhil Patel <nikhilpatel87@yahoo.com>
2021-03-15 12:43:22 -0500

    initial shop data set up

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/homepage/homepage';
+import ShopPage from './pages/shop/shop';
 
 const HatsPage = () => {
 	return (
@@ -16,7 +17,7 @@ function App() {
 		<div>
 			<Switch>
 				<Route exact path="/" component={HomePage} />
-				<Route path="/shop/hats" component={HatsPage} />
+				<Route path="/shop" component={ShopPage} />
 			</Switch>
 		</div>
 	);
