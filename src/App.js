06e55a6da6dc9123104a625caf936e1596a44f5b
Nikhil Patel <nikhilpatel87@yahoo.com>
2021-03-16 21:02:50 -0500

    added signin and sign out page, and sign-in component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import Header from './components/header/header';
 import HomePage from './pages/homepage/homepage';
 import ShopPage from './pages/shop/shop';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-out/sign-in-and-sign-out';
 
 function App() {
 	return (
@@ -12,6 +13,7 @@ function App() {
 			<Switch>
 				<Route exact path="/" component={HomePage} />
 				<Route path="/shop" component={ShopPage} />
+				<Route path="/signin" component={SignInAndSignUpPage} />
 			</Switch>
 		</div>
 	);
