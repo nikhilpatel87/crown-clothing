c57c92de03e0c662fea00c9e5f25273bee1e388a
Nikhil Patel <nikhilpatel87@yahoo.com>
2021-03-29 14:00:57 -0500

    added cartIcon and cart-dropdown components

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -8,7 +8,7 @@ import { auth } from '../../firebase/firebase.utils';
 import CartIcon from '../cart-icon/cart-icon';
 import CartDropdown from '../cart-dropdown/cart-dropdown';
 
-const Header = ({ currentUser }) => {
+const Header = ({ currentUser, hidden }) => {
 	return (
 		<div className="header">
 			<Link className="logo-container" to="/">
@@ -37,13 +37,14 @@ const Header = ({ currentUser }) => {
 				)}
 				<CartIcon />
 			</div>
-			<CartDropdown />
+			{hidden ? null : <CartDropdown />}
 		</div>
 	);
 };
 
-const mapStateToProps = (state) => ({
-	currentUser: state.user.currentUser,
+const mapStateToProps = ({ user: { currentUser }, cart: { hidden } }) => ({
+	currentUser,
+	hidden,
 });
 
 export default connect(mapStateToProps)(Header);
